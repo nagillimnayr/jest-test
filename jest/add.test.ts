fa5e6e0c17377f51d54b1c791bb2f08ae5e0c25b
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-27 15:32:16 -0700

    feat: add 'describe' to add.test.ts

---
+++ jest/add.test.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+import { describe, expect, test } from '@jest/globals';
+import add from './add';
+
+describe('add module', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+  });
+});
