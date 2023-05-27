3765f5c45052c1456f13ea030c0604e0d5d0eab9
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-27 14:34:17 -0700

    build: install typescript eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
 /** @format */
 
 module.exports = {
-	extends: [
-		'eslint:recommended',
-		'plugin:import/recommended',
-		'eslint-config-airbnb-base',
-		'prettier',
-	],
-	settings: {
-		'import/resolver': {
-			node: {
-				paths: ['src'],
-				extensions: ['.js', '.jsx', '.ts', '.tsx'],
-			},
-		},
-	},
-	env: {
-		browser: true,
-		node: true,
-	},
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'eslint-config-airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  root: true,
 };
--- jest/add.test.js
+++ jest/add.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-const add = require('./add');
+import add from './add';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
